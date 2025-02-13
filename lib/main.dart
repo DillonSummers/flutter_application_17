51e38ab85a945aa0c73a7d30c8c102ba45a54046
DillonSummers <157649377+DillonSummers@users.noreply.github.com>
2025-02-13 12:13:22 -0600

    Update1 main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -96,6 +96,44 @@ class SelectionScreen extends StatelessWidget {
   }
 }
 
+class SelectionScreen extends StatelessWidget {
+  const SelectionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick an option'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context, 'Yep');
+                },
+                child: const Text('Yep!'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context, 'Nope');
+                },
+                child: const Text('Nope.'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 
 class SelectionScreen extends StatelessWidget {
   const SelectionScreen({super.key});
